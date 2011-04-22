e4a61e04bbc0a0d9e4930bea393d92c29d6cb9a8
Goliath <goliathdp@gmail.com>
2011-04-22 10:22:04 +0300

    Fix tabs

--- EventDispatcher.php
+++ EventDispatcher.php
@@ -9,58 +9,53 @@ require_once dirname(__FILE__)."/Event.php";
 class EventDispatcher implements IEventDispatcher
 {
  
- 	private $listeners;
- 	
- 	public function __construct()
- 	{
- 	    $this->listeners = array();
- 	}
+     private $listeners;
+     
+    public function __construct()
+    {
+        $this->listeners = array();
+    }
  
- 	/**
- 	 * $this->addEventListener("Init", array(&$this, "f"));
+	/**
+	 * $this->addEventListener("Init", array(&$this, "f"));
 	 * $this->addEventListener("Init", ty);
- 	 */
- 	public function addEventListener($type, $listener)
- 	{
- 		if (!is_callable($listener)) {
- 			return false;
+	 */
+    public function addEventListener($type, $listener)
+    {
+        if (!is_callable($listener)) {
+            return false;
         }
-		
+        
         $key = $this->_getKey($listener);
-		
+        
         if (isset($this->listeners[$type][$key])) {
             return false;
         }
 
         $this->listeners[$type][$key] = $listener;
-		return true;
- 	} // end addEventListener
- 	
- 	public function dispatchEvent(Event $event)
- 	{
- 	    if (empty($this->listeners[$event->type])) {
- 	        return false;
- 	    }
- 	    
+        return true;
+    } // end addEventListener
+     
+    public function dispatchEvent(Event $event)
+    {
+        if (empty($this->listeners[$event->type])) {
+            return false;
+        }
+         
         foreach ($this->listeners[$event->type] as $key => $listener) {
             if (is_callable($listener)) {
                 call_user_func_array($listener, array($event));
             } 
         }
-		
+        
         return true;
- 	} // end dispatchEvent
- 	
- 	public function hasEventListener($type)
- 	{
- 		return !empty($this->listeners[$type]);
- 	} 
- 	
- 	public function removeEventListener($type, $listener)
- 	{
- 	    $key = $this->_getKey($listener);
-    	unset($this->listeners[$type][$key]);
- 	}
+    } // end dispatchEvent
+     
+    public function removeEventListener($type, $listener)
+    {
+        $key = $this->_getKey($listener);
+        unset($this->listeners[$type][$key]);
+    }
 
     private function _getKey($listener)
     {
@@ -72,6 +67,6 @@ class EventDispatcher implements IEventDispatcher
         
         return $key;
     } // end _getKey
- 	
+     
 }
 ?>
--- IEventDispatcher.php
+++ IEventDispatcher.php
@@ -1,17 +1,14 @@
 <?php
 /**
  * 
- * author: Denis Panaskin
+ * author: Denis Panaskin <goliathdp@gmail.com>
  * version: Beta 1.0 28.04.2007
  */
- 
-
- 
- interface IEventDispatcher
- {
- 	public function addEventListener($type, $listener);
- 	public function dispatchEvent(Event $event); 
- 	public function hasEventListener($type); 
- 	public function removeEventListener($type, $listener); 
- }
+interface IEventDispatcher
+{
+    public function addEventListener($type, $listener);
+    public function dispatchEvent(Event $event); 
+    public function hasEventListener($type); 
+    public function removeEventListener($type, $listener); 
+}
 ?>
